328bdb20c6dfd082eab05f5c8160f520e1ff698c
Álvaro Ortiz <alvaro@furilo.com>
2015-04-22 10:53:56 +0200

    Adding BCC for notifications

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
     @user = user
     @user_name = user.name
     @user_url  = user_url(user)
-    mail(to: @user.email, subject: t('mailer.lets_start', user_name: @user_name, user_url: @user_url))
+    mail(to: @user.email, bcc: "alvaro+reasons@furilo.com", subject: t('mailer.lets_start', user_name: @user_name, user_url: @user_url))
   end
 
   def new_vote_on_your_reason(voter, reason)
@@ -14,7 +14,7 @@ class UserMailer < ApplicationMailer
     @user_name = reason.user.name
     @reason = reason.title
     @reason_url = issue_reason_url(reason.issue_id, reason)
-    mail(to: reason.user.email, subject: t('mailer.new_vote_on_your_reason_subject', user_name: @user_name, voter: @voter, reason: @reason, reason_url: @reason_url))
+    mail(to: reason.user.email, bcc: "alvaro+reasons@furilo.com", subject: t('mailer.new_vote_on_your_reason_subject', user_name: @user_name, voter: @voter, reason: @reason, reason_url: @reason_url))
   end
 
   def new_reason_on_your_issue(voter, issue)
@@ -22,7 +22,7 @@ class UserMailer < ApplicationMailer
     @user_name = issue.user.name
     @issue = issue.title
     @issue_url = issue_url(issue)
-    mail(to: issue.user.email, subject: t('mailer.new_reason_on_your_issue_subject', user_name: @user_name, voter: @voter, issue: @issue, issue_url: @issue_url))
+    mail(to: issue.user.email, bcc: "alvaro+reasons@furilo.com", subject: t('mailer.new_reason_on_your_issue_subject', user_name: @user_name, voter: @voter, issue: @issue, issue_url: @issue_url))
   end
 
   def new_vote_on_your_issue(voter, reason)
@@ -33,5 +33,14 @@ class UserMailer < ApplicationMailer
     @issue_url = issue_url(reason.issue)
     mail(to: reason.issue.user.email, subject: t('mailer.new_vote_on_your_issue_subject', user_name: @user_name, voter: @voter, reason: @reason, issue: @issue, issue_url: @issue_url))
   end
+
+  # def new_club(voter, reason)
+  #   @voter = voter.name
+  #   @user_name = reason.issue.user.name
+  #   @reason = reason.title
+  #   @issue = reason.issue.title
+  #   @issue_url = issue_url(reason.issue)
+  #   mail(to: reason.issue.user.email, subject: t('mailer.new_vote_on_your_issue_subject', user_name: @user_name, voter: @voter, reason: @reason, issue: @issue, issue_url: @issue_url))
+  # end
 
 end
