f19b655394ea3abc847def19a13367838e37a5ba
Álvaro Ortiz <alvaro@furilo.com>
2015-04-25 01:04:54 +0200

    After vote share prompt

--- app/controllers/reasons_controller.rb
+++ app/controllers/reasons_controller.rb
@@ -4,7 +4,6 @@ class ReasonsController < ApplicationController
   before_action :load_reason, only: [:show, :vote, :unvote]
 
   def new
-    logger.info 'si entra reason path'
     @reason = Reason.new
   end
 
