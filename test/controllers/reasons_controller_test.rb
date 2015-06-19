51c41d81ae2140147f1b583a8eea71fb94d748a5
Bertocq <bertocq@gmail.com>
2015-06-19 11:54:05 +0200

    Remove /test directory since using rspec now

--- test/controllers/reasons_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ReasonsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/get_access_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GetAccessTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
