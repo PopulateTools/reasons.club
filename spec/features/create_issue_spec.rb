18433e7cfe0e6d95fde6e72941eb91c75843af8d
Bertocq <bertocq@gmail.com>
2015-07-01 15:06:55 +0200

    Improve Create Issue Spec with common function

--- spec/features/create_issue_spec.rb
+++ spec/features/create_issue_spec.rb
@@ -2,27 +2,30 @@ require 'rails_helper'
 
 RSpec.feature 'Create an Issue ' do
 
-  scenario 'should log with twitter first' do
-    visit home_page
-    page.click_link('', :href => '/issues/new')
-    expect(page).to have_content('Connect with Twitter to become a Reasoner') 
+  scenario 'a user should log with twitter before creating an issue' do
+    create_issue('Should I stay or should I go now?')
+    expect(page).to have_content("You need to sign in or sign up before continuing.") 
   end
 
-  scenario 'should create a public Issue and then go to its page' do
+  scenario 'a logged user can create a public Issue and then view its page' do
     create_session
-    my_club_title = 'Should I stay or should I go now?'
-    
+    create_issue('Reasons to eat Cachopo everyday', 'issue_privacy_public_2')
+    expect(page).to have_content("Hooray, your Club has been created. Time to start reasoning") 
+    expect(page).to have_content('Reasons to eat Cachopo everyday') 
+  end
+
+  private 
+
+  def create_issue(title, privacy = nil)
     visit home_page
-    page.click_link('', :href => '/issues/new')
+    page.click_link('add_new_issue')
     
     expect(page).to have_content('Set up your Club') 
     within(:css, '#new_issue') do
-      fill_in 'issue[title]', with: my_club_title
-      page.choose('issue_privacy_public_2')
+      fill_in 'issue[title]', with: title
+      page.choose(privacy ? privacy : 'issue_privacy_public_2')
       click_button 'Create your Club'
     end
-    expect(page).to have_content("Hooray, your Club has been created. Time to start reasoning") 
-    expect(page).to have_content(my_club_title) 
   end
 
 end
