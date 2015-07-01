90fc613b3ea32cb679edbcf7a903a6c30a2907f3
Bertocq <bertocq@gmail.com>
2015-07-01 11:46:47 +0200

    Just cleaning some lines

--- spec/features/omniauth_login_spec.rb
+++ spec/features/omniauth_login_spec.rb
@@ -1,25 +1,22 @@
 require 'rails_helper'
 
-RSpec.feature 'OmniAuth login' do
+RSpec.feature 'OmniAuth ' do
 
    
-  scenario "login existing twitter user" do
+  scenario 'login existing twitter user' do
     create_session
     expect(page).to have_content('@henry Henry Felguerosu')
   end
 
-  scenario "create and login a twitter user successfully" do
+  scenario 'create and login a twitter user successfully' do
     create_new_twitter_user
-
     expect(page).to have_content('@henry Henry Felguerosu')
   end
 
-  scenario "should create, login and logout successfully" do
+  scenario 'should create, login and logout successfully' do
     create_new_twitter_user
-    
     page.click_link('', :href => '/login/logout')
-    
-    expect(page).to have_content("Connect with") 
+    expect(page).to have_content('Connect with') 
   end
 
 end
