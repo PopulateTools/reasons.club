ed38c2b2aec1c9124919edbf67f8f762930a70ca
Bertocq <bertocq@gmail.com>
2015-07-02 17:30:20 +0200

    Improved omniauth test scenario descriptions

--- spec/features/omniauth_login_spec.rb
+++ spec/features/omniauth_login_spec.rb
@@ -3,19 +3,22 @@ require 'rails_helper'
 RSpec.feature 'OmniAuth ' do
 
    
-  scenario 'login existing twitter user' do
+  scenario 'login existing user with twitter' do
     create_session
+    #Signed in suscesfully???
     expect(page).to have_content('@henry Henry Felguerosu')
   end
 
-  scenario 'create and login a twitter user successfully' do
+  scenario 'create and login a user with twitter successfully' do
     create_new_twitter_user
+    #Signed in suscesfully???
     expect(page).to have_content('@henry Henry Felguerosu')
   end
 
   scenario 'should create, login and logout successfully' do
     create_new_twitter_user
     page.click_link('', :href => '/login/logout')
+    #Signed out successfully.
     expect(page).to have_content('Connect with') 
   end
 
