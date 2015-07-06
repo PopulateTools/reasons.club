f710fbfb6b00726ce0db6aef86d9b7d39db40d70
Bertocq <bertocq@gmail.com>
2015-07-06 16:45:28 +0200

    Complete reasons creating test

--- spec/features/create_reason_spec.rb
+++ spec/features/create_reason_spec.rb
@@ -6,6 +6,7 @@ RSpec.feature 'Create a Reason in a Issue ' do
     create_session
     create_issue_helper('Reasons to eat Cachopo everyday', 'issue_privacy_public_2')
   end
+ 
   scenario 'should create a "for" Reason and be able to see it', :js => true do
     within('div.reasons-for') {
       fill_in 'reason_title', :with => 'Improve my opinions in a structured way'
@@ -13,14 +14,12 @@ RSpec.feature 'Create a Reason in a Issue ' do
     }
     expect(page).to have_content('Improve my opinions in a structured way')
   end 
-=begin
 
-  scenario 'should create a "against" Reason and be able to see it' do
+  scenario 'should create an "against" Reason and be able to see it', :js => true do
     within('div.reasons-against') {
       fill_in 'reason_title', :with => 'I could be eating a Cachopo right now'
-      click_button('Send your reason')
+      page.execute_script("$('form#new_reason').submit()")
     }
     expect(page).to have_content('I could be eating a Cachopo right now')
-  end  
-=end
+  end 
 end
